40ec1edbe254fa9a282dc59d31ca99dc8a3c42da
krinish291 <radadiyakrinish1999@gmail.com>
2022-04-25 18:46:48 +0530

    Added: script

--- App/Screens/Components/Search/Search.js
+++ App/Screens/Components/Search/Search.js
@@ -3,9 +3,16 @@ import {SafeAreaView} from 'react-native';
 import {CustomText} from '../../CommonComponent';
 import CommonStyle from '../../../Theme/CommonStyle';
 import {AppContext} from '../../../AppContext';
+import Video from 'react-native-video';
+import {height, width} from '../../../Utils/Constant';
+
+const welcome = {
+  video: require('../../../Theme/welcome.mp4'),
+};
 
 const Search = props => {
   const {appTheme} = useContext(AppContext);
+  let player = React.useRef(null);
 
   return (
     <SafeAreaView
@@ -14,9 +21,22 @@ const Search = props => {
         CommonStyle.center,
         {backgroundColor: appTheme.background},
       ]}>
-      <CustomText xlarge style={{color: appTheme.text}}>
-        Search Tab
-      </CustomText>
+      <Video
+        {...props}
+        source={{
+          uri: 'https://file-examples.com/storage/fe69f82402626533c98f608/2017/04/file_example_MP4_480_1_5MG.mp4',
+        }}
+        ref={ref => {
+          player = ref;
+        }}
+        onError={error => console.log('error', error)}
+        repeat={true}
+        muted={true}
+        pictureInPicture={true}
+        disableFocus={true}
+        resizeMode={'cover'}
+        style={{height: height, width: width}}
+      />
     </SafeAreaView>
   );
 };
